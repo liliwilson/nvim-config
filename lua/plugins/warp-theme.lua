b84864dc0f138f3b35f6f1c04e7eff6fad4b4408
Lili Wilson <lmwilson@mit.edu>
2024-09-20 16:13:23 -0400

    split warp theme plugin into own repo

--- lua/plugins/warp-theme.lua
+++ lua/plugins/warp-theme.lua
@@ -1,12 +1,9 @@
 return {
-    dir = '~/.config/nvim/lua/warp-theme',
+    'liliwilson/warp-theme-nvim',
     config = function()
-        -- can either be a link to raw user content or path to yaml on home machine
-        local theme_link = "/Users/liliwilson/.warp/themes/hack_mit_2024.yaml"
-        --local theme_link =
-        --"https://raw.githubusercontent.com/warpdotdev/themes/refs/heads/main/warp_bundled/gruvbox_dark.yaml"
-        require('warp-theme').setup(
-            { theme_link = theme_link }
-        )
+        require('warp-theme-nvim').setup({
+            theme_link = "/Users/liliwilson/.warp/themes/hack_mit_2024.yaml"
+            -- theme_link = "https://raw.githubusercontent.com/warpdotdev/themes/refs/heads/main/warp_bundled/gruvbox_dark.yaml"
+        })
     end
 }
